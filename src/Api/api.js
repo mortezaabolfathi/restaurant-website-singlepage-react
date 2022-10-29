6246617ef670edf418b56bf486ba9d798f0a8f72
Morteza <morteza.abolfathi73@gmail.com>
2022-10-29 22:50:29 +0330

    update api.js get upcomingGame and newGame

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,4 +1,3 @@
-// api key : ac95f1754dd84cf184194b539ba272c6
 const API_KEY= "ac95f1754dd84cf184194b539ba272c6";
 const base_url = "https://api.rawg.io/api/"
 
@@ -32,6 +31,10 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`
 const LastYear = `${currentYear-1}-${currentMonth}-${currentDay}`
 const NextYear = `${currentYear+1}-${currentMonth}-${currentDay}`
 
-const popular_games=`games?key=${API_KEY}&dates=${LastYear},${currentDate}&ordering-rating&page_size=10`;
+const popular_games=`games?key=${API_KEY}&dates=${LastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games=`games?key=${API_KEY}&dates=${currentDate},${NextYear}&ordering=-added&page_size=10`;
+const new_games=`games?key=${API_KEY}&dates=${LastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`
+export const upcomingGames = () => `${base_url}${upcoming_games}`
+export const newGamesURL = () => `${base_url}${new_games}`
